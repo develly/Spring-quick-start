b9ecdd4108115ca71777d9384a04e016c16edba3
yurilee <projyuri@gmail.com>
2021-05-04 00:18:44 +0900

    Edit setter injection을 어노테이션 방식으로 수정(UserDAO, UserServiceImpl, applicationContext)

--- src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
+++ src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -1,17 +1,23 @@
 package com.springbook.biz.user.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.springbook.biz.user.UserService;
 import com.springbook.biz.user.UserVO;
 
+@Service("userService")
 public class UserServiceImpl implements UserService{
+	@Autowired
 	private UserDAO userDAO;
 
-	/**
-	 * @param userDAO the userDAO to set
-	 */
-	public void setUserDAO(UserDAO userDAO) {
-		this.userDAO = userDAO;
-	}
+// annotation으로 인해 필요없어짐 
+//	/**
+//	 * @param userDAO the userDAO to set
+//	 */
+//	public void setUserDAO(UserDAO userDAO) {
+//		this.userDAO = userDAO;
+//	}
 
 	public UserVO getUser(UserVO vo) {
 		// TODO Auto-generated method stub
