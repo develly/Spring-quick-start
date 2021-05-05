897c65af3f75f4502c201861571be72a70946297
yurilee <projyuri@gmail.com>
2021-05-05 15:06:08 +0900

    Add LogAdvice에 JoinPoint.getArgs() 추가

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -5,7 +5,9 @@ import org.aspectj.lang.JoinPoint;
 public class LogAdvice {
 	public void printLog(JoinPoint jp) {
 		String methodName = jp.getSignature().getName();
-		
-		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작, 호출한 method 이름: " + methodName);
+		Object[] args = jp.getArgs();
+	
+		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작, 호출한 method 이름: " + methodName
+				+ ", Args :" + args[0].toString());
 	}
 }
