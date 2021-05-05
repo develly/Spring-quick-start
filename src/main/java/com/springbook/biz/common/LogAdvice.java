6b64a5e2675984300882452433ac66415f7296b6
yurilee <projyuri@gmail.com>
2021-05-05 14:06:28 +0900

    Add LogAdvice에 JoinPoint 객체 추가

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,7 +1,11 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class LogAdvice {
-	public void printLog() {
-		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
+	public void printLog(JoinPoint jp) {
+		String methodName = jp.getSignature().getName();
+		
+		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작, 호출한 method 이름: " + methodName);
 	}
 }
