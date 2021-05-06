98ab23218e86363c4ff96413061f4800fd7e32e0
yurilee <projyuri@gmail.com>
2021-05-06 17:29:29 +0900

    Update AroundAdvice.java(annotation 방식으로 AOP설정)

--- src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,11 +1,21 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StopWatch;
 
 // AroundAdvice는 비즈니스 메소드의 호출을 가로챔
 // 클라이언트 호출한 비즈니스 메소드 호출하기 위해서 proceedinJoinpoint 객체를 매개변수로 받아야함
+@Service
+@Aspect
 public class AroundAdvice {
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@Around("allPointcut()") //바인드 변수가 없을 때는 이렇게 선언. 있으면 pointcut 사용 
 	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
 		System.out.println("[Around BEFORE]: 비즈니스 메소드 수행 전에 처리할 내용");
 		String method = pjp.getSignature().getName();
